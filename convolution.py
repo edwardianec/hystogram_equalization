61bd12203182d454339ef0d4b30bc549329ca6e3
edward sannikov <esannikov@k-lab.su>
2019-12-16 15:52:06 +0300

    convolution added

--- convolution.py
+++ convolution.py
@@ -38,21 +38,30 @@ def build_kernels(comb, kernel_size):
 	
 	nparray = []
 	for variant in list_variants:
-		new_arr = np.array( [np.array(variant[i*kernel_size:(i*kernel_size)+kernel_size], dtype="uint8") for i in range(0,kernel_size)], dtype="uint8")
+		new_arr = np.array( [np.array(variant[i*kernel_size:(i*kernel_size)+kernel_size], dtype="int8") for i in range(0,kernel_size)], dtype="int8")
 		nparray.append(new_arr)
 
 	return nparray
 
 
-def process_image(image_filename_path, write_path, kernel, kernel_position):
+def process_image(img_sum_t0, img_sum_t1, image_filename_path, write_path, kernel, kernel_position):
 	image_filename 		= image_filename_path.split("\\")[-1]
 
 	img 				= cv2.imread(image_filename_path,  cv2.IMREAD_GRAYSCALE)
-	height, width 		= img.shape	
+
 
 	conv_image	 		= cv2.filter2D(img,-1,kernel)
+	height, width 		= conv_image.shape	
+	img_sum				= 0
 
-	cv2.imwrite(write_path+str(kernel_position)+".tif", conv_image)
+	img_sum 			= cv2.sumElems(conv_image)
+	#print("conv_sum ", img_sum[0] )
+	#print("{0}:{1}".format(kernel_position, [str(i) for i in kernel]))
+	if (img_sum[0] == img_sum_t1[0]):
+		print("{0}:{1}".format(kernel_position, [str(i) for i in kernel]))
+		cv2.imwrite(write_path+str(kernel_position)+".tif", conv_image)
+
+	
 
 
 
@@ -62,15 +71,28 @@ def process_image(image_filename_path, write_path, kernel, kernel_position):
 def main():
 
 	image_filename_path		= "D:\\resilio\\ip_lib_ed\\src_images\\dslr\\tif\\_MG_9778.TIF"
+	target_image_0			= "D:\\resilio\\ip_lib_ed\\src_images\\dslr\\tif\\_MG_9778_t0.TIF"
+	target_image_1			= "D:\\resilio\\ip_lib_ed\\src_images\\dslr\\tif\\_MG_9778_t1.TIF"
+
+	t_img0 				= cv2.imread(target_image_0,  cv2.IMREAD_GRAYSCALE)
+	t_img1 				= cv2.imread(target_image_1,  cv2.IMREAD_GRAYSCALE)
+
+
+	img_sum_t0 			= cv2.sumElems(t_img0)
+	img_sum_t1 			= cv2.sumElems(t_img1)
+	print(img_sum_t0[0], "or ", img_sum_t1[0] )
+	
 	write_path				= "D:\\resilio\\ip_lib_ed\\src_images\\dslr\\tif\\processed\\"
 	kernel_position 			= 0
-	kernels 				= build_kernels(comb=[0,1,2],kernel_size=3)
+	kernels 				= build_kernels(comb=[-1,-3, 0,1,2,5],kernel_size=3)
 	print("total variants: ", len(kernels))	
+	
 
 	for kernel in kernels:
-		print("{0}:{1}".format(kernel_position, [str(i) for i in kernel]))
-		process_image(image_filename_path=image_filename_path, write_path=write_path, kernel=kernel, kernel_position=kernel_position)
+		
+		process_image(img_sum_t0, img_sum_t1, image_filename_path=image_filename_path, write_path=write_path, kernel=kernel, kernel_position=kernel_position)
 		kernel_position = kernel_position + 1
+		
 
 
 #concat_images = np.concatenate( (my_resize(img4,50), my_resize(img4, 50)), axis=1)
